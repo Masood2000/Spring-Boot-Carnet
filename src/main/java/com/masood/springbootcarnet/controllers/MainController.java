274e5d1d6d7f0c3bc8aa978e1338bb26d40fbe47
Masood2000 <masudurrehman2000@gmail.com>
2026-01-05 21:28:04 +0100

    module-start: 7.3

---
+++ src/main/java/com/masood/springbootcarnet/controllers/MainController.java
@@ -0,0 +1,15 @@
+package com.masood.springbootcarnet.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+
+    @RequestMapping("/home")
+    public String home(){
+        return "home.html";
+    }
+
+}
