2865283f4ac3740eb378950e9ff6a6d5846d1072
Masood2000 <masudurrehman2000@gmail.com>
2026-01-05 20:56:40 +0100

    first commit

---
+++ src/main/java/com/masood/springbootcarnet/SpringBootCarnetApplication.java
@@ -0,0 +1,13 @@
+package com.masood.springbootcarnet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCarnetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootCarnetApplication.class, args);
+    }
+
+}
