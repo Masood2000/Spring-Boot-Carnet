5f3d0e6d739854bfee512bd0871d0fb30371632b
Masood2000 <masudurrehman2000@gmail.com>
2026-01-07 12:21:51 +0100

    module-end: 7.3

--- src/main/java/com/masood/springbootcarnet/controllers/MainController.java
+++ src/main/java/com/masood/springbootcarnet/controllers/MainController.java
@@ -7,9 +7,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class MainController {
 
+
     @RequestMapping("/home")
     public String home(){
         return "home.html";
     }
+
 
 }
