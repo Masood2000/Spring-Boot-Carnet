a1ad980dd2be9a511aee2ef3187ca569ba93fd37
Masood2000 <masudurrehman2000@gmail.com>
2026-01-10 16:47:20 +0100

    module-start: 8.1

--- src/main/java/com/masood/springbootcarnet/controllers/MainController.java
+++ src/main/java/com/masood/springbootcarnet/controllers/MainController.java
@@ -1,14 +1,21 @@
 package com.masood.springbootcarnet.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MainController {
 
     @RequestMapping("/home")
-    public String home(){
+    public String home(Model page){
+
+
+        page.addAttribute("name","masood");
+        page.addAttribute("nationality","france");
+
         return "home.html";
+
     }
 
 }
