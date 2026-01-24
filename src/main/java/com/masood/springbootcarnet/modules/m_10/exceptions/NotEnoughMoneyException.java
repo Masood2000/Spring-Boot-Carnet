11557493193ec6632bde4620392e8d5b1064b5f0
Masood2000 <masudurrehman2000@gmail.com>
2026-01-25 00:16:03 +0100

    module-end: 10.3

--- src/main/java/com/masood/springbootcarnet/modules/m_10/exceptions/NotEnoughMoneyException.java
+++ src/main/java/com/masood/springbootcarnet/modules/m_10/exceptions/NotEnoughMoneyException.java
@@ -1,7 +1,5 @@
 package com.masood.springbootcarnet.modules.m_10.exceptions;
 
-
-
 public class NotEnoughMoneyException extends RuntimeException{
 
     public String message = "no enough balance";
