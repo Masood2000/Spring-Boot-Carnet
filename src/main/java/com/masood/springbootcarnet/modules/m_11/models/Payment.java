848d8531e076b64ec07fafb220c7783d9e9bfd5a
Masood2000 <masudurrehman2000@gmail.com>
2026-01-26 00:04:35 +0100

    module-start: 11.1

---
+++ src/main/java/com/masood/springbootcarnet/modules/m_11/models/Payment.java
@@ -0,0 +1,15 @@
+package com.masood.springbootcarnet.modules.m_11.models;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class Payment {
+
+    private String id;
+    private double amount;
+
+}
+
