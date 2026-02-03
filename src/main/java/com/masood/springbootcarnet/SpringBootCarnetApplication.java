5fcfac6fdcc620264bbe1bbd71d8ee338c945518
Masood2000 <masudurrehman2000@gmail.com>
2026-02-03 09:37:51 +0100

    module-resume: 14.3

--- src/main/java/com/masood/springbootcarnet/SpringBootCarnetApplication.java
+++ src/main/java/com/masood/springbootcarnet/SpringBootCarnetApplication.java
@@ -8,7 +8,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBootCarnetApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(SpringBootCarnetApplication.class, args);
+
     }
 
 }
