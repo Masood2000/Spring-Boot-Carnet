792966ddd258e1824eb709a9e31721f426a560c0
Masood2000 <masudurrehman2000@gmail.com>
2026-02-03 09:38:41 +0100

    module-resume: 14.3

--- src/main/java/com/masood/springbootcarnet/modules/m_14/model/Account.java
+++ src/main/java/com/masood/springbootcarnet/modules/m_14/model/Account.java
@@ -4,7 +4,6 @@ package com.masood.springbootcarnet.modules.m_14.model;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.Id;
-
 import java.math.BigDecimal;
 
 @Getter
