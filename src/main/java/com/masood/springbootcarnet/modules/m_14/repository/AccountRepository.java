4caafb5adabfa97aa2415c5f951371a96bbfec67
Masood2000 <masudurrehman2000@gmail.com>
2026-02-06 09:53:14 +0100

    module-end: 14.3

--- src/main/java/com/masood/springbootcarnet/modules/m_14/repository/AccountRepository.java
+++ src/main/java/com/masood/springbootcarnet/modules/m_14/repository/AccountRepository.java
@@ -5,10 +5,12 @@ import com.masood.springbootcarnet.modules.m_14.model.Account;
 import org.springframework.data.jdbc.repository.query.Modifying;
 import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
-
 import java.math.BigDecimal;
 import java.util.List;
 
+/***
+ * Simple JDBC Repository used by the Spring Data Project
+ */
 public interface AccountRepository extends CrudRepository<Account,Long> {
 
     @Query("SELECT * FROM account WHERE name = :name")
