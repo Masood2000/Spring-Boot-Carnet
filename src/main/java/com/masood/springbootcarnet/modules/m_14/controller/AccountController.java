f89d965109747f9a32a4c767c6659ec6992017d9
Masood2000 <masudurrehman2000@gmail.com>
2026-02-07 23:35:27 +0100

    module-end: 14.3

--- src/main/java/com/masood/springbootcarnet/modules/m_14/controller/AccountController.java
+++ src/main/java/com/masood/springbootcarnet/modules/m_14/controller/AccountController.java
@@ -6,7 +6,6 @@ import com.masood.springbootcarnet.modules.m_14.service.TransferService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-
 @RestController
 public class AccountController {
 
