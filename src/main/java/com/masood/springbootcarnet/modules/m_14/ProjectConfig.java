66acf5723807abf0b1fb8bd86746b09a2e07f190
Masood2000 <masudurrehman2000@gmail.com>
2026-02-09 17:17:30 +0100

    module-end: 14.3

--- src/main/java/com/masood/springbootcarnet/modules/m_14/ProjectConfig.java
+++ src/main/java/com/masood/springbootcarnet/modules/m_14/ProjectConfig.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import javax.sql.DataSource;
 
-
 /***
  * For this I use Hikari Datasource for JDBC
  */
