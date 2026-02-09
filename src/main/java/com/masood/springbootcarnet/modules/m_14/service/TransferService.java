da441d26571487ed51f05a47ef36d055834fff03
Masood2000 <masudurrehman2000@gmail.com>
2026-02-09 17:19:40 +0100

    module-start: 15.1

--- src/main/java/com/masood/springbootcarnet/modules/m_14/service/TransferService.java
+++ src/main/java/com/masood/springbootcarnet/modules/m_14/service/TransferService.java
@@ -11,17 +11,17 @@ import org.springframework.transaction.annotation.Transactional;
 import java.math.BigDecimal;
 import java.util.List;
 
-@Service
+//@Service
 public class TransferService {
 
     private final AccountRepository mAccountRepository;
 
-    @Autowired
+    //@Autowired
     public TransferService(AccountRepository _accountRepository) {
         this.mAccountRepository = _accountRepository;
     }
 
-    @Transactional
+    //@Transactional
     public void transferMoney(
             long idSender,
             long idReceiver,
