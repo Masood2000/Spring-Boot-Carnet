25d2e3bd5ea97a1b8b640067a97c104467daa24d
Masood2000 <masudurrehman2000@gmail.com>
2026-02-11 11:24:07 +0100

    module-start: 15.1

--- src/main/java/com/masood/springbootcarnet/modules/m_9/controllers/LoginController.java
+++ src/main/java/com/masood/springbootcarnet/modules/m_9/controllers/LoginController.java
@@ -1,6 +1,5 @@
 package com.masood.springbootcarnet.modules.m_9.controllers;
 
-
 import com.masood.springbootcarnet.modules.m_9.utils.LoginProcessor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
