855660202af4936bdedd5083b358a2dae9470696
Masood2000 <masudurrehman2000@gmail.com>
2026-02-12 15:26:32 +0100

    module-start: 15.1

--- src/main/java/com/masood/springbootcarnet/modules/m_8/controllers/MainController.java
+++ src/main/java/com/masood/springbootcarnet/modules/m_8/controllers/MainController.java
@@ -1,6 +1,5 @@
 package com.masood.springbootcarnet.modules.m_8.controllers;
 
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
