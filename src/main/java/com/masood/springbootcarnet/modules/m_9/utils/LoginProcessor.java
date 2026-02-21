6f5e202b8f11f366fbb1732c8bb204c102f7e813
Masood2000 <masudurrehman2000@gmail.com>
2026-02-21 21:18:56 +0100

    module-start: 15.1

--- src/main/java/com/masood/springbootcarnet/modules/m_9/utils/LoginProcessor.java
+++ src/main/java/com/masood/springbootcarnet/modules/m_9/utils/LoginProcessor.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.annotation.RequestScope;
 
-
 @Component
 @RequestScope
 public class LoginProcessor {
