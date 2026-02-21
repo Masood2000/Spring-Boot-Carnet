a8dbbddc572f7307f7e3d43a1766aa5bbfaca80f
Masood2000 <masudurrehman2000@gmail.com>
2026-02-21 21:18:28 +0100

    module-start: 15.1

--- src/main/java/com/masood/springbootcarnet/modules/m_14/service/TransferService.java
+++ src/main/java/com/masood/springbootcarnet/modules/m_14/service/TransferService.java
@@ -31,7 +31,6 @@ public class TransferService {
         this.mAccountRepository.changeAmount(idReceiver, receiverNewAmount);
     }
 
-
     public Iterable<Account> getAllAccounts() {
         return this.mAccountRepository.findAll();
     }
