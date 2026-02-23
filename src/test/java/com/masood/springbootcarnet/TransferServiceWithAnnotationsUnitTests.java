30393dcc4e0b227bc43f23fe39a0e097c6783579
Masood2000 <masudurrehman2000@gmail.com>
2026-02-23 04:27:09 +0100

    module-resume: 15.1

--- src/test/java/com/masood/springbootcarnet/TransferServiceWithAnnotationsUnitTests.java
+++ src/test/java/com/masood/springbootcarnet/TransferServiceWithAnnotationsUnitTests.java
@@ -8,7 +8,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-
 import java.math.BigDecimal;
 import java.util.Optional;
 import static org.mockito.BDDMockito.given;
