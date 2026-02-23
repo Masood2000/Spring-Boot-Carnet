3eb1c302177f532b6df7df842a902149335a46b5
Masood2000 <masudurrehman2000@gmail.com>
2026-02-23 04:51:38 +0100

    module-resume: 15.1

--- src/main/java/com/masood/springbootcarnet/SpringBootCarnetApplication.java
+++ src/main/java/com/masood/springbootcarnet/SpringBootCarnetApplication.java
@@ -9,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
 
+
 @SpringBootApplication(
       //  exclude = DataSourceAutoConfiguration.class
 )
